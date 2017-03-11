3e9074e832e84aabd19e6edbbf11c69a63a7efb2
wzdxt <wzdxt0624@gmail.com>
2017-03-12 00:02:33 +0800

    save

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -1,6 +1,11 @@
 class MonitorsController < ApplicationController
   def index
     @data={}
-
+    redis = Redis::Store::Factory.create('lianjia-redis')
+    keys = %w(process:ershoufang process:travel_xiaoqu)
+    values = redis.mget(keys)
+    keys.each_with_index do |key, index|
+      @data[key] = values[index]
+    end
   end
 end
